103778c487f49bb570f15fff174dc298dbd02bcf
oskarvedel <oskar.vedel@gmail.com>
2024-03-11 12:35:05 +0100

    smallfix

--- tdp-seo-text/texts/meta-description.php
+++ tdp-seo-text/texts/meta-description.php
@@ -2,7 +2,7 @@
 
 $meta_description_candidates = array(
     'Find opbevaring og opmagasinering i [location]. ✓ Alle opbevaringsrum i [location] ✓ Sammenlign priser ➡ Find den bedste løsning.', //default
-    'Find opbevarings- og depotrum i [location] hos [num_of_seo_gd_places] lagerhoteller.', //default (ejendomstorvet)
+    'Find opbevaringsrum og depotrum i [location] hos [num_of_seo_gd_places] lagerhoteller.', //default (ejendomstorvet)
     'Søg og book [seo_num_of_units_available] depotrum til leje hos [num_of_seo_gd_places] lagerhoteller i [location] ✓ Alle opbevaringsrum i [location] ✓ Sammenlign priser og find den bedste løsning.', //default (fra lokalebasen)
     '[num_of_seo_gd_places] lagerhoteller med mere end [num of units available] depotrum i [location]', //rentcafe
     'Opvarmede depotrum koster typisk fra from [lowest_price] om måneden for et lille 2 m² rum til [highest_price] om måneden for et større 10 m²rum. Book opbevaring på tjekdepot i dag!' //SpareFoot

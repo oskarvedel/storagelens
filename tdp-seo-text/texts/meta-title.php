9b3525320883e636931c3b500d0d7e02e70d043e
oskarvedel <oskar.vedel@gmail.com>
2024-03-18 18:28:02 +0100

    meta desc and title

--- tdp-seo-text/texts/meta-title.php
+++ tdp-seo-text/texts/meta-title.php
@@ -1,11 +1,11 @@
 <?php
 
 $meta_title_candidates = array(
-    'Se ALLE Opbevaringsrum [location] • Find ledige depotrum her', //default (dall)
+    'Tjek priser på alle depotrum i [location] • Find alle pulterrum her', //default (dall)
 
-    'Se ALLE opbevaringsrum i [location] • [seo_num_of_units_available] depotrum fra [lowest price] kr', //default (dall)
+    'Tjek priser på alle depotrum i [location] • [seo_num_of_units_available] rum fra [lowest price] kr', //default (dall)
 
-    'Opbevaringsrum [location] • [num_of_seo_gd_places] lagerhoteller',
+    'Tjek priser på alle opbevaringsrum i [location] • [num_of_seo_gd_places] lagerhoteller',
 
     '15 Billige Depotrum i [location] • Find Opbevaring i [location]', //gammel
     'Sammenlign Opbevaring i [location] ([num_of_seo_gd_places] lagerhoteller) • Find Depotrum i [location]', //gammel

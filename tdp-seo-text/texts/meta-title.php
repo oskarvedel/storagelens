8609ab87cd50f09805a0e7d25e8e70389f54efb4
oskarvedel <oskar.vedel@gmail.com>
2024-03-26 11:28:35 +0100

    meta titles

--- tdp-seo-text/texts/meta-title.php
+++ tdp-seo-text/texts/meta-title.php
@@ -1,11 +1,11 @@
 <?php
 
 $meta_title_candidates = array(
-    'Tjek priser på alle depotrum i [location] • Find alle pulterrum her', //default (dall)
+    'Se priser på ALLE depotrum i [location] • Find alle pulterrum her', //default (dall)
 
-    'Tjek priser på alle depotrum i [location] • [seo_num_of_units_available] rum fra [lowest price] kr', //default (dall)
+    'Se priser på ALLE depotrum i [location] • [seo_num_of_units_available] rum fra [lowest price] kr', //default (dall)
 
-    'Tjek priser på alle opbevaringsrum i [location] • [num_of_seo_gd_places] lagerhoteller',
+    'Se priser på ALLE opbevaringsrum i [location] • [num_of_seo_gd_places] lagerhoteller',
 
     '15 Billige Depotrum i [location] • Find Opbevaring i [location]', //gammel
     'Sammenlign Opbevaring i [location] ([num_of_seo_gd_places] lagerhoteller) • Find Depotrum i [location]', //gammel
